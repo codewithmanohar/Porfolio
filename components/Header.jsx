ba21a9b3b9385f0bed930449f3c04aefb3bab084
Manohar Kumar <icsixyz@gmail.com>
2025-11-10 18:52:23 +0530

    Updated Header Section

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,14 +25,22 @@ const Header = () => {
                             </a>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
-
+                    
                     <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <a href="/project">
+                                <Home className="w-5 h-5 sm:w-6 sm:h-6" />
+                            </a>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+
+                    {/* <NavigationMenuItem>
                         <NavigationMenuLink asChild>
                             <a href="/project">
                                 <RiCodeSSlashFill className="w-5 h-5 sm:w-6 sm:h-6" />
                             </a>
                         </NavigationMenuLink>
-                    </NavigationMenuItem>
+                    </NavigationMenuItem> */}
 
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
